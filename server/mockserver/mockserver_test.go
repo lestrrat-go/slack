b3b94ed4a3f1879506d4ad9847425db609b8ddbf
Daisuke Maki <lestrrat+github@gmail.com>
2017-11-15 13:56:51 +0900

    Fix eample

--- server/mockserver/mockserver_test.go
+++ server/mockserver/mockserver_test.go
@@ -29,7 +29,39 @@ func ExampleMockServer() {
 		return
 	}
 
-	json.NewEncoder(os.Stdout).Encode(channel)
+	buf, _ := json.MarshalIndent(channel, "", "  ")
+	os.Stdout.Write(buf)
 	// OUTPUT:
-	// {"id":"123456789ABCDEFG","created":233431200,"is_open":false,"creator":"yoda","is_archived":false,"is_group":false,"is_mpim":false,"members":["obiwan","lukeskywalker"],"name":"jedis","name_normalized":"jedis","num_members":2,"previous_names":null,"purpose":{"value":"There is no emotion, there is peace.\nThere is no ignorance, there is knowledge.\nThere is no passion, there is serenity.\nThere is no chaos, there is harmony.\nThere is no death, there is the Force.","creator":"yoda","last_set":233431200},"topic":{"value":"Jedi meetup and drinks next Tuesday","creator":"yoda","last_set":233431200},"is_channel":false,"is_general":false,"is_member":false,"is_org_shared":false,"is_shared":false}
+	//{
+	//   "id": "123456789ABCDEFG",
+	//   "created": 233431200,
+	//   "is_open": false,
+	//   "creator": "yoda",
+	//   "is_archived": false,
+	//   "is_group": false,
+	//   "is_mpim": false,
+	//   "members": [
+	//     "obiwan",
+	//     "lukeskywalker"
+	//   ],
+	//   "name": "jedis",
+	//   "name_normalized": "jedis",
+	//   "num_members": 2,
+	//   "previous_names": null,
+	//   "purpose": {
+	//     "value": "There is no emotion, there is peace.\nThere is no ignorance, there is knowledge.\nThere is no passion, there is serenity.\nThere is no chaos, there is harmony.\nThere is no death, there is the Force.",
+	//     "creator": "yoda",
+	//     "last_set": 233431200
+	//   },
+	//   "topic": {
+	//     "value": "Jedi meetup and drinks next Tuesday",
+	//     "creator": "yoda",
+	//     "last_set": 233431200
+	//   },
+	//   "is_channel": true,
+	//   "is_general": false,
+	//   "is_member": true,
+	//   "is_org_shared": false,
+	//   "is_shared": false
+	//}
 }
