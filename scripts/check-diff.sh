c66d68ceff9b5beeea09be09fb89c975fd77e7f2
Daisuke Maki <lestrrat+github@gmail.com>
2018-01-19 09:59:30 +0900

    Check untracked files too

--- scripts/check-diff.sh
+++ scripts/check-diff.sh
@@ -1,13 +1,32 @@
 #!/bin/sh
 
+UNTRACKED=$(git ls-files --others --exclude-standard)
 DIFF=$(git diff)
 
-if [ -z "$DIFF" ]; then
+if [ -z "$DIFF" && -z "$UNTRACKED" ]; then
     exit 0
 fi
 
-echo "$DIFF"
-echo ""
-echo "Above diff was found."
+if [ ! -z "$DIFF" ]; then
+    echo "==== START OF DIFF FOUND ==="
+    echo ""
+    echo "$DIFF"
+    echo ""
+    echo "Above diff was found."
+    echo ""
+    echo "==== END OF DIFF FOUND ==="
+    echo ""
+fi
+
+if [ ! -z "$UNTRACKED" ]; then
+    echo "==== START OF UNTRACKED FILES FOUND ==="
+    echo ""
+    echo "$UNTRACKED"
+    echo ""
+    echo "Above untracked files were found."
+    echo ""
+    echo "==== END OF UNTRACKED FILES FOUND ==="
+    echo ""
+fi
 
 exit 1
